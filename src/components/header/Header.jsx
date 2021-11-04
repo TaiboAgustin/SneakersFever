e8d7e51a4445f6dc2f1eb1968ed042d08ad5a473
Taibo Agustin <taibocruz@gmail.com>
2021-11-04 17:06:35 -0300

    agregando navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const header = () =>{
         <nav>
             <label className="logo">SneakersFever</label>
             <ul>
-                <li><a className="active" href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
                 <li><a href="#">Store</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">Contact</a></li>
