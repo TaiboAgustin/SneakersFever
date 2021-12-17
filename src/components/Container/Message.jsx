db35db58c145dc0c6e8ab892689a735edfc1f86a
Agustin <taibocruz@gmail.com>
2021-12-17 20:31:19 -0300

    arreglando componente message

--- src/components/Container/Message.jsx
+++ src/components/Container/Message.jsx
@@ -3,7 +3,6 @@ import './Message.css'
 
 const Message = ({ ord }) =>{
     const productName = ord.items.map((ticket) => ticket.name)
-    const productPrice = ord.items.map((ticket) => ticket.price)
     const { buyer } = ord
 
 
@@ -11,8 +10,7 @@ const Message = ({ ord }) =>{
         <React.Fragment>
             <div className="containerMessage">
                 <h2>Buy Date: <span>{ord.date}</span> </h2>
-                <h2>Product Name: <span>{productName}</span></h2>
-                <h2>Price: <span>${productPrice}</span></h2>
+                <h2>Product Name: <span>{productName} </span></h2>
                 <h2>Buy Id: <span>{ord.id}</span></h2>
                 <h2>Email: <span>{buyer}</span></h2>
             </div>    
